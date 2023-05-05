91607f59e54c647e90dfd7ce1efba774690bc7cd
Awointa <akosawointa@gmail.com>
2023-05-05 23:52:34 +0100

    first and second validation

--- script.js
+++ script.js
@@ -1,22 +1,90 @@
 "use strict ";
+// universal variables
+const btn = document.querySelector(".icon");
+const inputDay = document.querySelector(".input--day");
+const inputMonth = document.querySelector(".input--month");
+const inputYear = document.querySelector(".input--year");
+
+//Date elements
+let currentDate = new Date();
+let currentDay = currentDate.getDay();
+let currentMonth = currentDate.getMonth() + 1;
+let currentYear = currentDate.getFullYear();
+
+// error message elements
+const errorElements = document.querySelectorAll(".error-message");
+const inputElements = document.querySelectorAll(".input");
+const inputLabelElements = document.querySelectorAll(".input-label");
+const inputDayElementError = document.getElementById("day-error");
+const inputMonthElementError = document.getElementById("month-error");
+const inputYearElementError = document.getElementById("year-error");
+
+const resultElements = document.querySelectorAll(".output--number");
+
+let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+let messages = [];
+const validateMonthDays = (day, givenMonth) => {
+  const daysInMonth = month[givenMonth - 1];
+  if (day > daysInMonth) {
+    messages.push("Must be a valid date");
+    inputDayElementError.textContent = messages.join(", ");
+  }
+};
+
+const validateAllDates = (givenDay, givenMonth, givenYear) => {
+  if (
+    givenDay > 31 ||
+    (givenDay == 0 && givenMonth > month.length) ||
+    (givenMonth == 0 && givenYear > 2023)
+  )
+    messages = [
+      "Must be a valid day",
+      "Must be a valid month",
+      "Must be in the past",
+    ];
+};
 
 const displayDate = function (className, value) {
   document.querySelector(className).innerHTML = value;
 };
 
-const btn = document.querySelector(".icon");
 btn.addEventListener("click", function (e) {
   e.preventDefault();
-  let birthDay = Number(document.querySelector(".input--day").value);
-  let birthMonth = Number(document.querySelector(".input--month").value);
-  let birthYear = Number(document.querySelector(".input--year").value);
 
-  let currentDate = new Date();
-  let currentDay = currentDate.getDay();
-  let currentMonth = currentDate.getMonth() + 1;
-  let currentYear = currentDate.getFullYear();
+  let birthDay = Number(inputDay.value);
+  let birthMonth = Number(inputMonth.value);
+  let birthYear = Number(inputYear.value);
 
-  month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  // error messages
+  if (birthDay == "" && birthMonth == "" && birthYear == "")
+    messages.push("This field is required");
+  errorElements.forEach((errElement) => {
+    errElement.textContent = messages.join(", ");
+  });
+
+  validateMonthDays(birthDay, birthMonth);
+
+  validateAllDates(birthDay, birthMonth, birthYear);
+
+  console.log(messages);
+
+  if (messages.length > 0) {
+    inputElements.forEach((input) => (input.style.borderColor = "#ff5757")),
+      inputLabelElements.forEach(
+        (inputLabel) => (inputLabel.style.color = "#ff5757"),
+        (messages = [])
+      );
+  } else {
+    inputElements.forEach((input) => (input.style.borderColor = "#dbdbdb")),
+      inputLabelElements.forEach(
+        (inputLabel) => (inputLabel.style.color = "#716f6f")
+      );
+  }
+
+  console.log(messages);
+
+  //calculator Logic
 
   if (birthDay > currentDay) {
     currentDay += month[currentDay - 1];
@@ -30,19 +98,7 @@ btn.addEventListener("click", function (e) {
   let currentAgeInMonths = currentMonth - birthMonth;
   let currentAgeInYears = currentYear - birthYear;
 
-  console.log(currentAgeInYears);
-
   displayDate(".output--days", currentAgeInDays);
   displayDate(".output--months", currentAgeInMonths);
   displayDate(".output--years", currentAgeInYears);
 });
-
-// ulate the difference in years var years = today.getFullYear () - birthDate.getFullYear ();
-
-// Calculate the difference in months var months = today.getMonth () - birthDate.getMonth ();
-
-// Adjust for leap years and incomplete months if (today.getDate () < birthDate.getDate ()) { months–; }
-
-// Add 12 months if the birth month is after the current month if (months < 0) { years–; months += 12; }
-
-// Display the result console.log (“You are " + years + " years and " + months + " months old.”)
